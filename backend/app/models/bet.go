ea3e8c8fc3a9de236f5c8991d92eb505c385f0f2
igor.silveira <igor-silveira10@hotmail.com>
2022-09-26 18:09:10 -0300

    feat: create bet struct

---
+++ backend/app/models/bet.go
@@ -0,0 +1,27 @@
+package models
+
+import "time"
+
+type Bet struct {
+	ID            string    `json:"id" validate:"required,uuid" gorm:"type:uuid;primary_key"`
+	CreatedAt     time.Time `json:"created_at"`
+	NationalTeamA string    `json:"national_team_a" validate:"required,uuid" gorm:"type:varchar(255)"`
+	NationalTeamB string    `json:"national_team_b" validate:"required,uuid" gorm:"type:varchar(255)"`
+	GolA          int8      `json:"gol_a" validate:"required" gorm:"type:integer"`
+	GolB          int8      `json:"gol_b" validate:"required" gorm:"type:integer"`
+	UserID        string    `json:"user_id" validate:"required,uuid" gorm:"type:varchar(255)"`
+	MatchID       string    `json:"match_id" validate:"required,uuid" gorm:"type:varchar(255)"`
+}
+
+func NewBet() *Bet {
+	return &Bet{}
+}
+
+func (bet *Bet) Validate() error {
+	err := validate.Struct(bet)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
