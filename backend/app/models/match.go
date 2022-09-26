94f2ebcf9ea83d4399873d972ca931e7c776c1fb
igor.silveira <igor-silveira10@hotmail.com>
2022-09-26 17:55:57 -0300

    feat: add match struct

---
+++ backend/app/models/match.go
@@ -0,0 +1,26 @@
+package models
+
+import "time"
+
+type Match struct {
+	ID            string    `json:"id" validate:"required,uuid" gorm:"type:uuid;primary_key"`
+	Date          time.Time `json:"date"`
+	NationalTeamA string    `json:"national_team_a" validate:"required,uuid" gorm:"type:varchar(255)"`
+	NationalTeamB string    `json:"national_team_b" validate:"required,uuid" gorm:"type:varchar(255)"`
+	GolA          int8      `json:"gol_a" validate:"required" gorm:"type:integer"`
+	GolB          int8      `json:"gol_b" validate:"required" gorm:"type:integer"`
+	KeyID         string    `json:"key_id" validate:"required,uuid" gorm:"type:varchar(255)"`
+}
+
+func NewMatch() *Match {
+	return &Match{}
+}
+
+func (match *Match) Validate() error {
+	err := validate.Struct(match)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
