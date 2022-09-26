5a3c0e26168589edd8ff01f29dff4c64736b63f0
igor.silveira <igor-silveira10@hotmail.com>
2022-09-26 17:19:55 -0300

    feat: add user model

---
+++ backend/app/models/user.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"github.com/go-playground/validator/v10"
+	_ "github.com/go-playground/validator/v10"
+)
+
+type User struct {
+	ID      string `json:"id" validate:"required,uuid" gorm:"type:uuid;primary_key"`
+	Name    string `json:"name" validate:"required" gorm:"type:varchar(255)"`
+	Email   string `json:"email" validate:"required,email" gorm:"type:varchar(255)"`
+	IsAdmin *bool  `json:"is_admin" validate:"required" gorm:"type:bool;default:false"`
+}
+
+var validate = validator.New()
+
+func NewUser() *User {
+	return &User{}
+}
+
+func (user *User) Validate() error {
+	err := validate.Struct(user)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
