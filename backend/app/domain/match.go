40310ddce6b1abc791524d8ad6bc04fc3de65256
igor.silveira <igor-silveira10@hotmail.com>
2022-10-01 18:07:55 -0300

    feat: match should have BracketID and WinnerID

--- backend/app/domain/match.go
+++ backend/app/domain/match.go
@@ -9,7 +9,8 @@ type Match struct {
 	NationalTeamB string    `json:"national_team_b" validate:"required,uuid" gorm:"type:varchar(255)"`
 	GolA          int8      `json:"gol_a" validate:"required" gorm:"type:integer"`
 	GolB          int8      `json:"gol_b" validate:"required" gorm:"type:integer"`
-	KeyID         string    `json:"key_id" validate:"required,uuid" gorm:"type:varchar(255)"`
+	BracketID     string    `json:"bracket_id" validate:"required,uuid" gorm:"type:varchar(255)"`
+	WinnerID      string    `json:"winner_id" validate:"required,uuid" gorm:"type:varchar(255)"`
 }
 
 func NewMatch() *Match {
