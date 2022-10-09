33ed305e35b344d64095e51324ae7ef0593bb2a5
igor.silveira <igor-silveira10@hotmail.com>
2022-10-09 17:05:12 -0300

    feat: add national tea repository

---
+++ backend/app/repositories/national_team_repository.go
@@ -0,0 +1,49 @@
+package repositories
+
+import (
+	"cupcake/app/database"
+	"cupcake/app/domain"
+	"fmt"
+)
+
+type NationalTeamRepository interface {
+	Insert(nationalTeam *domain.NationalTeam) (*domain.NationalTeam, error)
+	Find(id string) (*domain.NationalTeam, error)
+	Update(nationalTeam *domain.NationalTeam) (*domain.NationalTeam, error)
+}
+
+type NationalTeamRepositoryDb struct {
+	Db *database.Database
+}
+
+func (repo NationalTeamRepositoryDb) Insert(nationalTeam *domain.NationalTeam) (*domain.NationalTeam, error) {
+	err := repo.Db.Create(nationalTeam).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return nationalTeam, nil
+}
+
+func (repo NationalTeamRepositoryDb) Find(id string) (*domain.NationalTeam, error) {
+	var nationalTeam domain.NationalTeam
+
+	repo.Db.First(&nationalTeam, "id = ?", id)
+
+	if nationalTeam.ID == "" {
+		return nil, fmt.Errorf("nationalTeam does not exist")
+	}
+
+	return &nationalTeam, nil
+}
+
+func (repo NationalTeamRepositoryDb) Update(nationalTeam *domain.NationalTeam) (*domain.NationalTeam, error) {
+	err := repo.Db.Save(&nationalTeam).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return nationalTeam, nil
+}
