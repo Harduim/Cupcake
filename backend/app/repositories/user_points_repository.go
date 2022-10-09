dc07f97c62334504a56262624b6c8fcccd2490fb
igor.silveira <igor-silveira10@hotmail.com>
2022-10-09 17:21:33 -0300

    feat: add user points repository

---
+++ backend/app/repositories/user_points_repository.go
@@ -0,0 +1,49 @@
+package repositories
+
+import (
+	"cupcake/app/database"
+	"cupcake/app/domain"
+	"fmt"
+)
+
+type UserPointsRepository interface {
+	Insert(userPoints *domain.UserPoints) (*domain.UserPoints, error)
+	Find(id string) (*domain.UserPoints, error)
+	Update(userPoints *domain.UserPoints) (*domain.UserPoints, error)
+}
+
+type UserPointsRepositoryDb struct {
+	Db *database.Database
+}
+
+func (repo UserPointsRepositoryDb) Insert(userPoints *domain.UserPoints) (*domain.UserPoints, error) {
+	err := repo.Db.Create(userPoints).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return userPoints, nil
+}
+
+func (repo UserPointsRepositoryDb) Find(id string) (*domain.UserPoints, error) {
+	var userPoints domain.UserPoints
+
+	repo.Db.First(&userPoints, "user_id = ?", id)
+
+	if userPoints.UserID == "" {
+		return nil, fmt.Errorf("user does not have points")
+	}
+
+	return &userPoints, nil
+}
+
+func (repo UserPointsRepositoryDb) Update(userPoints *domain.UserPoints) (*domain.UserPoints, error) {
+	err := repo.Db.Save(&userPoints).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return userPoints, nil
+}
