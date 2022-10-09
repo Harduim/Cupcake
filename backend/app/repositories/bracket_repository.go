649b009bf0e90cef5ec5e0de48ae5d42fda223d5
igor.silveira <igor-silveira10@hotmail.com>
2022-10-09 17:04:38 -0300

    feat: add bracket repository

---
+++ backend/app/repositories/bracket_repository.go
@@ -0,0 +1,49 @@
+package repositories
+
+import (
+	"cupcake/app/database"
+	"cupcake/app/domain"
+	"fmt"
+)
+
+type BracketRepository interface {
+	Insert(bracket *domain.Bracket) (*domain.Bracket, error)
+	Find(id string) (*domain.Bracket, error)
+	Update(bracket *domain.Bracket) (*domain.Bracket, error)
+}
+
+type BracketRepositoryDb struct {
+	Db *database.Database
+}
+
+func (repo BracketRepositoryDb) Insert(bracket *domain.Bracket) (*domain.Bracket, error) {
+	err := repo.Db.Create(bracket).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return bracket, nil
+}
+
+func (repo BracketRepositoryDb) Find(id string) (*domain.Bracket, error) {
+	var bracket domain.Bracket
+
+	repo.Db.First(&bracket, "id = ?", id)
+
+	if bracket.ID == "" {
+		return nil, fmt.Errorf("bracket does not exist")
+	}
+
+	return &bracket, nil
+}
+
+func (repo BracketRepositoryDb) Update(bracket *domain.Bracket) (*domain.Bracket, error) {
+	err := repo.Db.Save(&bracket).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return bracket, nil
+}
