977cc271eae35baa6146684a38311d4eb42beab6
igor.silveira <igor-silveira10@hotmail.com>
2022-10-19 19:53:29 -0300

    chore: add matches repository

---
+++ backend/app/controllers/match.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"cupcake/app/database"
+	"cupcake/app/domain"
+	"github.com/gofiber/fiber/v2"
+)
+
+// GetAllMatches Return all matches as JSON
+func GetAllMatches(db *database.Database) fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+		var Matches []domain.Match
+		if response := db.Find(&Matches); response.Error != nil {
+			panic("Error occurred while retrieving matches from the database: " + response.Error.Error())
+		}
+		err := ctx.JSON(Matches)
+		if err != nil {
+			panic("Error occurred when returning JSON of users: " + err.Error())
+		}
+		return err
+	}
+}
