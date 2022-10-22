ea37de66b0a30dc8778005af3abe94657ab21eb4
igor.silveira <igor-silveira10@hotmail.com>
2022-10-22 19:02:13 -0300

    chore: register brackets endpoints

--- backend/app/routes/api.go
+++ backend/app/routes/api.go
@@ -11,6 +11,7 @@ import (
 func RegisterRoutes(api fiber.Router, db *database.Database, sso *service.SSOClient) {
 	registerUsers(api, db)
 	registerAuth(api, db, sso)
+	registerBrackets(api, db)
 }
 
 func registerUsers(api fiber.Router, db *database.Database) {
@@ -26,4 +27,9 @@ func registerUsers(api fiber.Router, db *database.Database) {
 func registerAuth(api fiber.Router, db *database.Database, sso *service.SSOClient) {
 	users := api.Group("/auth")
 	users.Get("/sso", Controller.AuthenticateSSO(sso))
+}
+
+func registerBrackets(api fiber.Router, db *database.Database) {
+	brackets := api.Group("/brackets")
+	brackets.Get("/", Controller.GetAllBrackets(db))
 }
