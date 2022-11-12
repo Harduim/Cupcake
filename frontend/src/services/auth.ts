0fe6ef33cf22924eb662a454794cd9f62bb598f7
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-11-12 09:55:02 -0300

    Chore: changes were made

---
+++ frontend/src/services/auth.ts
@@ -0,0 +1,51 @@
+import { Buffer } from 'buffer'
+import Cookies from 'universal-cookie'
+
+const { REACT_APP_WEBSITE_DOMAIN } = process.env
+
+const TOKEN_KEY = '@TOKEN_KEY_CUPCAKE'
+
+interface IJWT {
+  uid: string
+  exp: number
+}
+
+const parseJWT = (jwt_string: string) => {
+  const buffer = Buffer.from(jwt_string.split('.')[1], 'base64')
+  const jwt = JSON.parse(buffer.toString()) as IJWT
+  return jwt
+}
+
+const isExpired = (expires: number) => {
+  return Math.floor(new Date().getTime() / 1000) <= expires
+}
+
+const isAuthenticated = () => {
+  return getToken() !== null
+}
+
+const getToken = () => {
+  const cookies = new Cookies()
+  const jwt_string = cookies.get(TOKEN_KEY) as string
+  const jwt = parseJWT(jwt_string)
+  return isExpired(jwt.exp) ? jwt : null
+}
+
+const login = (jwt_string: string) => {
+  const jwt = parseJWT(jwt_string)
+  const cookies = new Cookies()
+  cookies.set(TOKEN_KEY, jwt_string, {
+    path: '/',
+    maxAge: jwt.exp,
+    domain: REACT_APP_WEBSITE_DOMAIN,
+  })
+}
+
+const logout = () => {
+  const token = getToken()
+  if (!token) return
+  const cookies = new Cookies()
+  cookies.remove(TOKEN_KEY, { path: '/' })
+}
+
+export { isAuthenticated, getToken, login, logout }
