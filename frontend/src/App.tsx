935e5542d1b7ec321b03d5f051536324c2ee1659
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-11-12 14:58:38 -0300

    Feat: home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react'
+import { EuiProvider } from '@elastic/eui'
+import '@elastic/eui/dist/eui_theme_light.css'
 import './App.css'
-import logo from './logo.svg'
+import Home from './pages/home'
 
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <EuiProvider colorMode='light'>
+      <Home />
+    </EuiProvider>
   )
 }
 
---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,11 @@
+import PageLayout from '../components/PageLayout'
+
+const Home = () => {
+  return (
+    <PageLayout title='Página inicial' isLoading={false}>
+      <div>Content</div>
+    </PageLayout>
+  )
+}
+
+export default Home
