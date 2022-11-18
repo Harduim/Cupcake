556bf3abf611b190605273f1ee285507efad7aba
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-11-18 19:12:40 -0300

    Chore: export types

--- frontend/src/clients/index.ts
+++ frontend/src/clients/index.ts
@@ -1,4 +1,8 @@
 /* tslint:disable */
 /* eslint-disable */
 
+export type { Bet } from './models/Bet'
+export type { Bracket } from './models/Bracket'
+export type { Match } from './models/Match'
+export type { NationalTeam } from './models/NationalTeam'
 export type { User } from './models/User'
