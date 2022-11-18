f0bc0ef6d3ded1b064cf79346722c0a4be045dd4
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-11-18 00:54:11 -0300

    Chore: rename pkg

--- backend/app/repositories/national_team_repository.go
+++ backend/app/repositories/national_team_repository.go
@@ -2,22 +2,22 @@ package repositories
 
 import (
 	"cupcake/app/database"
-	domain "cupcake/app/models"
+	"cupcake/app/models"
 	"fmt"
 )
 
 type NationalTeamRepository interface {
-	Insert(nationalTeam *domain.NationalTeam) (*domain.NationalTeam, error)
-	Find(id string) (*domain.NationalTeam, error)
-	FindAll() ([]*domain.NationalTeam, error)
-	Update(nationalTeam *domain.NationalTeam) (*domain.NationalTeam, error)
+	Insert(nationalTeam *models.NationalTeam) (*models.NationalTeam, error)
+	Find(id string) (*models.NationalTeam, error)
+	FindAll() ([]*models.NationalTeam, error)
+	Update(nationalTeam *models.NationalTeam) (*models.NationalTeam, error)
 }
 
 type NationalTeamRepositoryDb struct {
 	Db *database.Database
 }
 
-func (repo NationalTeamRepositoryDb) Insert(nationalTeam *domain.NationalTeam) (*domain.NationalTeam, error) {
+func (repo NationalTeamRepositoryDb) Insert(nationalTeam *models.NationalTeam) (*models.NationalTeam, error) {
 	err := repo.Db.Create(nationalTeam).Error
 
 	if err != nil {
@@ -27,8 +27,8 @@ func (repo NationalTeamRepositoryDb) Insert(nationalTeam *domain.NationalTeam) (
 	return nationalTeam, nil
 }
 
-func (repo NationalTeamRepositoryDb) Find(id string) (*domain.NationalTeam, error) {
-	var nationalTeam domain.NationalTeam
+func (repo NationalTeamRepositoryDb) Find(id string) (*models.NationalTeam, error) {
+	var nationalTeam models.NationalTeam
 
 	repo.Db.First(&nationalTeam, "id = ?", id)
 
@@ -39,15 +39,15 @@ func (repo NationalTeamRepositoryDb) Find(id string) (*domain.NationalTeam, erro
 	return &nationalTeam, nil
 }
 
-func (repo NationalTeamRepositoryDb) FindAll() (*[]domain.NationalTeam, error) {
-	var nationalTeams []domain.NationalTeam
+func (repo NationalTeamRepositoryDb) FindAll() (*[]models.NationalTeam, error) {
+	var nationalTeams []models.NationalTeam
 
 	repo.Db.Find(&nationalTeams)
 
 	return &nationalTeams, nil
 }
 
-func (repo NationalTeamRepositoryDb) Update(nationalTeam *domain.NationalTeam) (*domain.NationalTeam, error) {
+func (repo NationalTeamRepositoryDb) Update(nationalTeam *models.NationalTeam) (*models.NationalTeam, error) {
 	err := repo.Db.Save(&nationalTeam).Error
 
 	if err != nil {
