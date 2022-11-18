e61fd7641b18f5e22b0421b5686f72c5192cfcda
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-11-18 01:02:54 -0300

    Chore: tweak json

--- backend/app/models/match.go
+++ backend/app/models/match.go
@@ -5,8 +5,8 @@ import "time"
 type Match struct {
 	ID              string        `json:"id" validate:"required,uuid" gorm:"type:uuid;primary_key"`
 	Date            time.Time     `json:"date" validate:"required"`
-	NationalTeamAID *string       `json:"nationalTeamA" validate:"uuid" gorm:"type:varchar(255)"`
-	NationalTeamBID *string       `json:"nationalTeamB" validate:"uuid" gorm:"type:varchar(255)"`
+	NationalTeamAID *string       `json:"nationalTeamAId" validate:"uuid" gorm:"type:varchar(255)"`
+	NationalTeamBID *string       `json:"nationalTeamBId" validate:"uuid" gorm:"type:varchar(255)"`
 	NationalTeamA   *NationalTeam `gorm:"foreignKey:NationalTeamAID"`
 	NationalTeamB   *NationalTeam `gorm:"foreignKey:NationalTeamBID"`
 	GolA            *int8         `json:"golA" gorm:"type:integer"`
