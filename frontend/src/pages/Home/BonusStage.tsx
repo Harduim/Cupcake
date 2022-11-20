756e307fa876a9c16c7ad0f9657954d94da4e9a3
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-11-20 13:17:52 -0300

    Feat: bonus stage layout

--- frontend/src/pages/Home/BonusStage.tsx
+++ frontend/src/pages/Home/BonusStage.tsx
@@ -1,5 +1,50 @@
+import {
+  EuiCard,
+  EuiFlexGrid,
+  EuiFlexItem,
+  EuiIcon,
+  EuiPanel,
+  EuiSpacer,
+  EuiTitle,
+} from '@elastic/eui'
+import { useContext, useState } from 'react'
+import { Match } from '../../clients'
+import MatchForm from '../../components/MatchForm'
+import GlobalContext from '../../context/GlobalContext'
+
 const BonusStage = () => {
-  return <></>
+  const { teams, isLoading } = useContext(GlobalContext)
+
+  const coringaMatch: Match = {
+    id: 'coringa-match',
+    name: 'Finais (Coringa)',
+    date: '2022-12-03',
+    nationalTeamAId: '',
+    nationalTeamBId: '',
+    golA: 0,
+    golB: 0,
+    bracketId: '',
+    winnerId: '',
+  }
+
+  if (isLoading || !teams) return null
+  return (
+    <EuiFlexGrid columns={2} gutterSize='m'>
+      <EuiPanel>
+        <EuiTitle size='xs'>
+          <h2>Tente Acertar a Final</h2>
+        </EuiTitle>
+        <EuiSpacer size='m' />
+        <MatchForm
+          match={coringaMatch}
+          teams={teams}
+          onSubmit={match => {
+            console.log(match)
+          }}
+        />
+      </EuiPanel>
+    </EuiFlexGrid>
+  )
 }
 
 export default BonusStage
