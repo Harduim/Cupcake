b497923306ecbd6d9a2fa08d04154b035f462c28
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-11-20 02:33:52 -0300

    Chore: extract bet form

---
+++ frontend/src/pages/Home/BetForm.tsx
@@ -0,0 +1,166 @@
+import {
+  EuiAvatar,
+  EuiButton,
+  EuiCard,
+  EuiFieldNumber,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+  EuiPanel,
+  EuiSpacer,
+  EuiTitle,
+} from '@elastic/eui'
+import { useState } from 'react'
+import uuid from 'react-uuid'
+import { Bet, Match, NationalTeam } from '../../clients'
+import api, { queryClient } from '../../services/api'
+import { HOURS_BEFORE_MATCH_IN_MILLISECONDS } from '../../utils/constants'
+import { dateToBrDateTimeString, timeStringToBrDateTimeString } from '../../utils/datetime'
+
+const { PUBLIC_URL } = process.env
+
+const updateBet = async (bet: Bet, update: boolean) => {
+  console.log(update ? 'update' : 'create', bet)
+  try {
+    if (update) {
+      await api.put('bets', bet)
+    } else {
+      await api.post('bets', bet)
+    }
+  } catch (error) {
+    console.error(error)
+    return
+  }
+  queryClient.invalidateQueries(['bets'])
+}
+
+interface IBetProps {
+  match: Match
+  teamMap: Map<string, NationalTeam>
+  bets?: Map<string, Bet>
+}
+
+const BetForm = ({ match, teamMap, bets }: IBetProps) => {
+  const bet = bets?.get(match.id)
+  let defaultBet: Bet = bet || { id: uuid(), matchId: match.id }
+  const [_bet, setBet] = useState<Bet>(defaultBet)
+
+  const handleChange = (prop: string, value: string | number | undefined) => {
+    if (value === undefined) return
+    const newBet = { ..._bet, [prop]: value }
+    setBet(newBet)
+  }
+  const matchClose = new Date(Date.parse(match.date) - HOURS_BEFORE_MATCH_IN_MILLISECONDS)
+  const nationalTeamA = teamMap.get(match.nationalTeamAId)
+  const nationalTeamB = teamMap.get(match.nationalTeamBId)
+  const isDisabled =
+    !match.nationalTeamAId || !match.nationalTeamBId || matchClose.getTime() < new Date().getTime()
+
+  const iconNotDefined = <EuiAvatar size='xl' name='N' />
+  const iconTeamA = <EuiIcon size='xxl' type={`${PUBLIC_URL}/flags/${nationalTeamA?.name}.svg`} />
+  const iconTeamB = <EuiIcon size='xxl' type={`${PUBLIC_URL}/flags/${nationalTeamB?.name}.svg`} />
+
+  const getDisplay = (teamId?: string, winnerId?: string) => {
+    let display: 'success' | 'accent' | 'subdued'
+    if (!winnerId || !teamId) {
+      display = 'subdued'
+    } else if (_bet.winnerId === teamId) {
+      display = 'success'
+    } else {
+      display = 'accent'
+    }
+    return display
+  }
+
+  const displayTeamA = getDisplay(match.nationalTeamAId, _bet?.winnerId)
+  const displayTeamB = getDisplay(match.nationalTeamBId, _bet?.winnerId)
+
+  const isSendDisabled =
+    _bet.golA === undefined || _bet.golB === undefined || _bet.winnerId === undefined
+
+  return (
+    <EuiPanel>
+      <EuiTitle size='xs'>
+        <h2>{match.name}</h2>
+      </EuiTitle>
+      <EuiSpacer size='s' />
+      <p>
+        <b>Horário do jogo:</b> {timeStringToBrDateTimeString(match.date)}
+      </p>
+      <p>
+        <b>Encerramento:</b> &nbsp;&nbsp; {dateToBrDateTimeString(matchClose)}
+      </p>
+      <EuiSpacer size='m' />
+      <EuiFlexGroup justifyContent='spaceAround'>
+        <p>
+          <b>Escolha o Vencedor</b>
+        </p>
+      </EuiFlexGroup>
+      <EuiSpacer size='s' />
+      <EuiFlexGroup justifyContent='spaceAround'>
+        <EuiFlexItem>
+          <EuiCard
+            icon={!nationalTeamA ? iconNotDefined : iconTeamA}
+            title={nationalTeamA?.name.replace('_', ' ') || 'Não Definido'}
+            isDisabled={isDisabled}
+            onClick={() => handleChange('winnerId', nationalTeamA?.id)}
+            display={displayTeamA}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiCard
+            icon={!nationalTeamA ? iconNotDefined : iconTeamB}
+            title={nationalTeamB?.name.replace('_', ' ') || 'Não Definido'}
+            isDisabled={isDisabled}
+            onClick={() => handleChange('winnerId', nationalTeamB?.id)}
+            display={displayTeamB}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiSpacer size='s' />
+      <EuiFlexGroup justifyContent='spaceAround'>
+        <p>
+          <b>Defina o Placar</b>
+        </p>
+      </EuiFlexGroup>
+      <EuiSpacer size='s' />
+      <EuiFlexGroup>
+        <EuiFieldNumber
+          placeholder=' '
+          value={_bet.golA === undefined ? '' : _bet.golA}
+          disabled={isDisabled}
+          onChange={(e: any) => {
+            const gols = parseInt(e.target.value)
+            if (gols < 0 || Number.isNaN(gols)) return
+            handleChange('golA', gols)
+          }}
+        />
+        <EuiFieldNumber
+          placeholder=' '
+          value={_bet.golB === undefined ? '' : _bet.golB}
+          disabled={isDisabled}
+          onChange={(e: any) => {
+            const gols = parseInt(e.target.value)
+            if (gols < 0 || Number.isNaN(gols)) return
+            handleChange('golB', gols)
+          }}
+        />
+      </EuiFlexGroup>
+      <EuiSpacer size='s' />
+      <EuiSpacer size='m' />
+      <EuiButton
+        color='primary'
+        disabled={isSendDisabled}
+        onClick={() => {
+          updateBet(_bet, _bet.id === bet?.id)
+        }}
+        fill
+      >
+        Salvar
+      </EuiButton>
+      <EuiSpacer size='m' />
+    </EuiPanel>
+  )
+}
+
+export default BetForm
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,176 +1,17 @@
 import {
-  EuiAvatar,
-  EuiButton,
-  EuiCard,
-  EuiFieldNumber,
   EuiFlexGrid,
-  EuiFlexGroup,
   EuiFlexItem,
-  EuiIcon,
   EuiPageSection,
   EuiPanel,
+  EuiSelectable,
   EuiSpacer,
-  EuiText,
   EuiTitle,
 } from '@elastic/eui'
-import { useContext, useState } from 'react'
-import uuid from 'react-uuid'
-import { Bet, Match, NationalTeam } from '../../clients'
+import { useContext } from 'react'
 import PageLayout from '../../components/PageLayout'
 import GlobalContext from '../../context/GlobalContext'
-import api, { queryClient } from '../../services/api'
-import { HOURS_BEFORE_MATCH_IN_MILLISECONDS } from '../../utils/constants'
-import {
-  dateToBrDateTimeString,
-  timeStringToBrDateString,
-  timeStringToBrDateTimeString,
-} from '../../utils/datetime'
-
-const { PUBLIC_URL } = process.env
-
-const updateBet = async (bet: Bet, update: boolean) => {
-  console.log(update ? 'update' : 'create', bet)
-  try {
-    if (update) {
-      await api.put('bets', bet)
-    } else {
-      await api.post('bets', bet)
-    }
-  } catch (error) {
-    console.error(error)
-    return
-  }
-  queryClient.invalidateQueries(['bets'])
-}
-
-interface IBetProps {
-  match: Match
-  teamMap: Map<string, NationalTeam>
-  bets?: Map<string, Bet>
-}
-
-const BetForm = ({ match, teamMap, bets }: IBetProps) => {
-  const bet = bets?.get(match.id)
-  let defaultBet: Bet = bet || { id: uuid(), matchId: match.id }
-  const [_bet, setBet] = useState<Bet>(defaultBet)
-
-  const handleChange = (prop: string, value: string | number | undefined) => {
-    if (value === undefined) return
-    const newBet = { ..._bet, [prop]: value }
-    setBet(newBet)
-  }
-  const matchClose = new Date(Date.parse(match.date) - HOURS_BEFORE_MATCH_IN_MILLISECONDS)
-  const nationalTeamA = teamMap.get(match.nationalTeamAId)
-  const nationalTeamB = teamMap.get(match.nationalTeamBId)
-  const isDisabled =
-    !match.nationalTeamAId || !match.nationalTeamBId || matchClose.getTime() < new Date().getTime()
-
-  const iconNotDefined = <EuiAvatar size='xl' name='N' />
-  const iconTeamA = <EuiIcon size='xxl' type={`${PUBLIC_URL}/flags/${nationalTeamA?.name}.svg`} />
-  const iconTeamB = <EuiIcon size='xxl' type={`${PUBLIC_URL}/flags/${nationalTeamB?.name}.svg`} />
-
-  const getDisplay = (teamId?: string, winnerId?: string) => {
-    let display: 'success' | 'accent' | 'subdued'
-    if (!winnerId || !teamId) {
-      display = 'subdued'
-    } else if (_bet.winnerId === teamId) {
-      display = 'success'
-    } else {
-      display = 'accent'
-    }
-    return display
-  }
-
-  const displayTeamA = getDisplay(match.nationalTeamAId, _bet?.winnerId)
-  const displayTeamB = getDisplay(match.nationalTeamBId, _bet?.winnerId)
-
-  const isSendDisabled =
-    _bet.golA === undefined || _bet.golB === undefined || _bet.winnerId === undefined
-
-  return (
-    <EuiPanel>
-      <EuiTitle size='xs'>
-        <h2>{match.name}</h2>
-      </EuiTitle>
-      <EuiSpacer size='s' />
-      <p>
-        <b>Horário do jogo:</b> {timeStringToBrDateTimeString(match.date)}
-      </p>
-      <p>
-        <b>Encerramento:</b> &nbsp;&nbsp; {dateToBrDateTimeString(matchClose)}
-      </p>
-      <EuiSpacer size='m' />
-      <EuiFlexGroup justifyContent='spaceAround'>
-        <p>
-          <b>Escolha o Vencedor</b>
-        </p>
-      </EuiFlexGroup>
-      <EuiSpacer size='s' />
-      <EuiFlexGroup justifyContent='spaceAround'>
-        <EuiFlexItem>
-          <EuiCard
-            icon={!nationalTeamA ? iconNotDefined : iconTeamA}
-            title={nationalTeamA?.name.replace('_', ' ') || 'Não Definido'}
-            isDisabled={isDisabled}
-            onClick={() => handleChange('winnerId', nationalTeamA?.id)}
-            display={displayTeamA}
-          />
-        </EuiFlexItem>
-        <EuiFlexItem>
-          <EuiCard
-            icon={!nationalTeamA ? iconNotDefined : iconTeamB}
-            title={nationalTeamB?.name.replace('_', ' ') || 'Não Definido'}
-            isDisabled={isDisabled}
-            onClick={() => handleChange('winnerId', nationalTeamB?.id)}
-            display={displayTeamB}
-          />
-        </EuiFlexItem>
-      </EuiFlexGroup>
-      <EuiSpacer size='s' />
-      <EuiFlexGroup justifyContent='spaceAround'>
-        <p>
-          <b>Defina o Placar</b>
-        </p>
-      </EuiFlexGroup>
-      <EuiSpacer size='s' />
-      <EuiFlexGroup>
-        <EuiFieldNumber
-          placeholder=' '
-          value={_bet.golA === undefined ? '' : _bet.golA}
-          disabled={isDisabled}
-          onChange={(e: any) => {
-            const gols = parseInt(e.target.value)
-            if (gols < 0 || Number.isNaN(gols)) return
-            handleChange('golA', gols)
-          }}
-        />
-        <EuiFieldNumber
-          placeholder=' '
-          value={_bet.golB === undefined ? '' : _bet.golB}
-          disabled={isDisabled}
-          onChange={(e: any) => {
-            const gols = parseInt(e.target.value)
-            if (gols < 0 || Number.isNaN(gols)) return
-            handleChange('golB', gols)
-          }}
-        />
-      </EuiFlexGroup>
-      <EuiSpacer size='s' />
-      <EuiSpacer size='m' />
-      <EuiButton
-        color='primary'
-        disabled={isSendDisabled}
-        onClick={() => {
-          updateBet(_bet, _bet.id === bet?.id)
-        }}
-        fill
-      >
-        Salvar
-      </EuiButton>
-      <EuiSpacer size='m' />
-    </EuiPanel>
-  )
-}
+import { timeStringToBrDateString } from '../../utils/datetime'
+import BetForm from './BetForm'
 
 const Home = () => {
   const { brackets, bets, teamMap, isLoading } = useContext(GlobalContext)
@@ -184,6 +25,31 @@ const Home = () => {
   }
   return (
     <PageLayout title='Bolão Copa do Mundo 2022' isLoading={isLoading}>
+      <EuiPageSection color='subdued'>
+        <EuiTitle>
+          <h1>Fase de Grupos</h1>
+        </EuiTitle>
+        <EuiSpacer size='m' />
+        <EuiPanel>
+          <EuiSelectable
+            aria-label='Searchable example'
+            options={[{ label: 'Alpha' }, { label: 'B', searchableLabel: 'Beta' }]}
+            onChange={newOptions => {}}
+            searchable
+            searchProps={{
+              'data-test-subj': 'dataTestSubj',
+            }}
+          >
+            {(list, search) => (
+              <>
+                {search}
+                {list}
+              </>
+            )}
+          </EuiSelectable>
+        </EuiPanel>
+      </EuiPageSection>
+      <EuiSpacer size='m' />
       {brackets
         .sort((a, b) => Date.parse(a.openDate) - Date.parse(b.openDate))
         .map(b => {
