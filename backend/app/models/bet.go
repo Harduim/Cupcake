b5b03058c69564ec6723609414a4996b7bdbf324
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-11-20 00:41:19 -0300

    Chore: add todo

--- backend/app/models/bet.go
+++ backend/app/models/bet.go
@@ -7,16 +7,17 @@ import (
 )
 
 type Bet struct {
-	ID        string        `json:"id" validate:"required,uuid" gorm:"type:uuid;primary_key"`
-	CreatedAt time.Time     `json:"createdAt"`
-	GolA      *int          `json:"golA" validate:"required" gorm:"type:integer"`
-	GolB      *int          `json:"golB" validate:"required" gorm:"type:integer"`
-	UserID    string        `json:"userId" validate:"required,uuid" gorm:"type:varchar(255)"`
-	User      *User         `gorm:"foreignKey:UserID"`
-	MatchID   string        `json:"matchId" validate:"required,uuid" gorm:"type:varchar(255)"`
-	Match     *Match        `gorm:"foreignKey:MatchID"`
-	WinnerID  string        `json:"winnerId" validate:"required,uuid" gorm:"type:varchar(255)"`
-	Winner    *NationalTeam `gorm:"foreignKey:WinnerID"`
+	ID        string    `json:"id" validate:"required,uuid" gorm:"type:uuid;primary_key"`
+	CreatedAt time.Time `json:"createdAt"`
+	GolA      *int      `json:"golA" validate:"required" gorm:"type:integer"`
+	GolB      *int      `json:"golB" validate:"required" gorm:"type:integer"`
+	UserID    string    `json:"userId" validate:"required,uuid" gorm:"type:varchar(255)"`
+	User      *User     `gorm:"foreignKey:UserID"`
+	// TODO: add unique composto com matchId a userId
+	MatchID  string        `json:"matchId" validate:"required,uuid" gorm:"type:varchar(255)"`
+	Match    *Match        `gorm:"foreignKey:MatchID"`
+	WinnerID string        `json:"winnerId" validate:"required,uuid" gorm:"type:varchar(255)"`
+	Winner   *NationalTeam `gorm:"foreignKey:WinnerID"`
 }
 
 func NewBet(nationalTeamAID string,
