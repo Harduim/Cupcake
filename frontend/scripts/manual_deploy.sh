02ae768d9f0230581c581866bef38a35872d6621
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-11-21 01:48:59 -0300

    Chore: move to proper folder

--- frontend/scripts/manual_deploy.sh
+++ frontend/scripts/manual_deploy.sh
@@ -3,7 +3,7 @@ set -e
 cd frontend
 yarn build
 zip -r build.zip build
-scp build.zip $DEPLOY_USERNAME@$DEPLOY_HOSTNAME:~
+scp -P $DEPLOY_SSH_PORT build.zip $DEPLOY_USERNAME@$DEPLOY_HOSTNAME:~
 
 ssh $DEPLOY_USERNAME@$DEPLOY_HOSTNAME -p $DEPLOY_SSH_PORT "rm -rf /var/www/html/*"
 ssh $DEPLOY_USERNAME@$DEPLOY_HOSTNAME -p $DEPLOY_SSH_PORT 'unzip -o ~/build.zip'
