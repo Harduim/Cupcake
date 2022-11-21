0b56a6e266ecb8d8fa59ddf4f4899777ae82e92b
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-11-21 04:25:09 -0300

    Chore: re-deploy flags

--- frontend/scripts/manual_deploy.sh
+++ frontend/scripts/manual_deploy.sh
@@ -9,3 +9,5 @@ ssh $DEPLOY_USERNAME@$DEPLOY_HOSTNAME -p $DEPLOY_SSH_PORT 'unzip -o ~/build.zip'
 ssh $DEPLOY_USERNAME@$DEPLOY_HOSTNAME -p $DEPLOY_SSH_PORT "mv ~/build/* /var/www/html/"
 ssh $DEPLOY_USERNAME@$DEPLOY_HOSTNAME -p $DEPLOY_SSH_PORT "mv ~/build/.htaccess /var/www/html/"
 ssh $DEPLOY_USERNAME@$DEPLOY_HOSTNAME -p $DEPLOY_SSH_PORT 'rm ~/build.zip'
+
+scp -P $DEPLOY_SSH_PORT public/flags/*.svg $DEPLOY_USERNAME@$DEPLOY_HOSTNAME:/var/www/html/flags/
