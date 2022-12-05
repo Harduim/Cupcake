c2268f7232c1dfa2aedf302c04d40a4e2c19c3c6
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-12-05 00:08:53 -0300

    Chore> score calculator

--- misc/pontuacao.py
+++ misc/pontuacao.py
@@ -4,6 +4,7 @@ from sqlmodel.orm.session import Session
 from sqlalchemy.future.engine import Engine
 from dotenv import load_dotenv
 from dataclasses import dataclass
+from uuid import UUID
 
 load_dotenv()
 
@@ -12,6 +13,8 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_DATABASE = os.getenv("DB_DATABASE")
 DB_PORT = os.getenv("DB_PORT")
 DB_HOST = os.getenv("DB_HOST")
+
+OITAVAS_ID = UUID("ef13e77f-b345-4f4d-b4a7-2d1cfb12fa48")
 
 assert all([DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT, DB_HOST]), "Missing environment vars"
 
@@ -28,6 +31,8 @@ class Points:
     WINNER = 2
     SCOREBOARD_WINNER = 3
 
+    GROUP_STAGE = 3
+
 
 class SQLModel(_SQLModel):
     @classmethod
@@ -42,46 +47,35 @@ class SQLModel(_SQLModel):
 
 class Match(SQLModel, table=True):
     __tablename__ = "matches"
-    id: str = Field(primary_key=True)
+    id: UUID = Field(primary_key=True)
     name: str
-    national_team_a_id: str
-    national_team_b_id: str
+    national_team_a_id: UUID
+    national_team_b_id: UUID
     gol_a: int
     gol_b: int
-    bracket_id: str
-    winner_id: str
+    bracket_id: UUID
+    winner_id: UUID
 
 
 class NationalTeam(SQLModel, table=True):
     __tablename__ = "national_teams"
-    id: str = Field(primary_key=True)
+    id: UUID = Field(primary_key=True)
     name: str
 
 
 class Bet(SQLModel, table=True):
     __tablename__ = "bets"
-    id: str = Field(primary_key=True)
+    id: UUID = Field(primary_key=True)
     gol_a: int
     gol_b: int
-    user_id: str
-    match_id: str
-    winner_id: str
-
-
-class User(SQLModel, table=True):
-    __tablename__ = "users"
-    id: str = Field(primary_key=True)
-    name: str
-    points: int
-
-    def bets(self) -> list[Bet]:
-        with Session(conn) as db:
-            return db.exec(select(Bet).where(Bet.user_id == self.id)).all()
+    user_id: UUID
+    match_id: UUID
+    winner_id: UUID
 
 
 class Bracket(SQLModel, table=True):
     __tablename__ = "brackets"
-    id: str = Field(primary_key=True)
+    id: UUID = Field(primary_key=True)
     name: str
     multiplier: int
 
@@ -90,6 +84,27 @@ class GroupStage(SQLModel, table=True):
     __tablename__ = "groups"
     user_id: str = Field(primary_key=True)
     national_team_id: str = Field(primary_key=True)
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+    id: UUID = Field(primary_key=True)
+    name: str
+    points: int
+
+    def bets(self) -> list[Bet]:
+        with Session(conn) as db:
+            return db.exec(select(Bet).where(Bet.user_id == self.id)).all()
+
+    def group_stage_bets(self) -> list[GroupStage]:
+        with Session(conn) as db:
+            return db.exec(select(GroupStage).where(GroupStage.user_id == str(self.id))).all()
+
+    def update_score(self, score: int):
+        self.points = score
+        with Session(conn) as db:
+            db.add(self)
+            db.commit()
 
 
 def calculate_match_score(
@@ -140,20 +155,35 @@ def calculate_match_score(
 
 
 def calculate_score(
-    user: User,
-    teams: dict[str, NationalTeam],
-    brackets: dict[str, Bracket],
-    group_stage_points: int = 0,
+    user: User, teams: dict[str, NationalTeam], brackets: dict[str, Bracket], matches: list[Match]
 ):
+    round_16_matches = [m for m in matches if m.bracket_id == OITAVAS_ID]
+
+    round_16_teams = {
+        *{m.national_team_a_id for m in round_16_matches},
+        *{m.national_team_b_id for m in round_16_matches},
+    }
+    group_stage_bets = {UUID(gb.national_team_id) for gb in user.group_stage_bets()}
+    acertos = {g for g in group_stage_bets if g in round_16_teams}
+    group_stage_points = len(acertos) * Points.GROUP_STAGE
+
     final_score = group_stage_points
     user_bets = user.bets()
-    if len(user_bets) == 0:
-        return 0
+
+    def format_aposta(nat_teams: set[UUID]) -> str:
+        return ", ".join(sorted([teams[gb].name for gb in nat_teams]))
+
     print("\n" + user.name)
-    if group_stage_points>0:
-        print(f"\tPontos da fase de grupos: {group_stage_points}")
+    print("\n\tFase de grupos")
+    print(f"\tAposta:    {format_aposta(group_stage_bets)}")
+    print(f"\tResultado: {format_aposta(round_16_teams)}")
+    print(f"\t{len(acertos)} acertos: {format_aposta(acertos)}")
+    print(f"\tPontos da fase de grupos: {group_stage_points}")
+
+    match_map = {m.id: m for m in matches}
+
     for bet in user_bets:
-        match = matches[bet.match_id]
+        match = match_map[bet.match_id]
         if match.winner_id is None:
             # Skipping matches that did ont have happened yet
             continue
@@ -163,6 +193,8 @@ def calculate_score(
         print(f"\tPontuação anterior: {final_score}")
         final_score += score
         print(f"\tPontuação atualizada: {final_score}")
+
+    return final_score
 
 
 if __name__ == "__main__":
@@ -175,8 +207,9 @@ if __name__ == "__main__":
             DB_DATABASE,
         )
     )
-    matches: dict[str, Match] = Match.id_map()
+    matches = Match.all()
     teams: dict[str, NationalTeam] = NationalTeam.id_map()
     brackets: dict[str, Bracket] = Bracket.id_map()
     for user in User.all():
-        calculate_score(user, teams, brackets)
+        final_score = calculate_score(user, teams, brackets, matches)
+        user.update_score(final_score)
